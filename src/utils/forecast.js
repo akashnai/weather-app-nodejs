eabb0199fad52544487c02683b06b9755b94a10f
Akash Nai <akashnai632@gmail.com>
2022-08-20 15:19:33 +0530

    added high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         callback('Unable to find location', undefined)
     }
     else{
-        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + ' degrees out.')
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. But it feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
     }
 })
 
